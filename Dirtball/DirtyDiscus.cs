9d4deac2d9bb68d632faf9241776a74612a3f6fd
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-09-05 08:49:23 -0500

    Add files via upload

---
+++ Dirtball/DirtyDiscus.cs
@@ -0,0 +1,37 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Azercadmium.Items.Dirtball
+{
+	public class DirtyDiscus : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			Tooltip.SetDefault("Feeling it makes you feel dirty...\nRapidly throw discuses");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 12;
+			item.melee = true;
+			item.width = 33;
+			item.height = 33;
+			item.useTime = 31;
+			item.useAnimation = 31;
+			item.useStyle = ItemUseStyleID.HoldingUp;
+			item.knockBack = 1.5f;
+			item.value = 2000;
+			item.rare = -1;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.useTurn = true;
+			item.noMelee = true;
+			item.noUseGraphic = true;
+			item.shoot = mod.ProjectileType("DirtyDiscus");
+			item.shootSpeed = 12f;
+		}
+	}
+}
---
+++ Dirtball/DirtyPistol.cs
@@ -0,0 +1,43 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Azercadmium.Items.Dirtball
+{
+	public class DirtyPistol : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Paydirt Pistol");
+		}
+		public override void SetDefaults() {
+			item.value = Item.sellPrice(0, 1, 0, 0);
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.useAnimation = 17;
+			item.useTime = 17;
+			item.damage = 6;
+			item.width = 12;
+			item.height = 24;
+			item.knockBack = 1.5f;
+			item.shoot = ProjectileID.Bullet;
+			item.shootSpeed = 30f;
+			item.noMelee = true;
+			item.ranged = true;
+			item.useAmmo = AmmoID.Bullet;
+			item.UseSound = SoundID.Item11;
+			item.autoReuse = true;
+			item.rare = -1;
+		}
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
+			float numberProjectiles = 2;
+			float rotation = MathHelper.ToRadians(5);
+			position += Vector2.Normalize(new Vector2(speedX, speedY)) * 45f;
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f;
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+			}
+			return false;
+		}
+	}
+}
