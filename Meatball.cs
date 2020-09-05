035ad0345172e0766de85b78c6f37d08173d6e81
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-09-05 09:18:16 -0500

    Add files via upload

---
+++ Meatball.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Azercadmium.Projectiles
+{
+	public class Meatball : ModProjectile
+	{
+        public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Meatball");
+        }
+		public override void SetDefaults()
+		{
+			projectile.width = 39;
+			projectile.height = 39;
+			projectile.aiStyle = 1;
+			projectile.friendly = true;
+			projectile.penetrate = 5;
+			projectile.timeLeft = 1500;
+			projectile.ignoreWater = true;
+			projectile.light = 0f;
+			aiType = 1;
+		}
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			if (Main.rand.Next(5) == 0)
+		    target.AddBuff(BuffID.Poisoned, 240, false);
+		}
+		public override void Kill(int timeLeft)
+		{
+			Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+		}
+	}   
+}
---
+++ NailWeak.cs
@@ -0,0 +1,33 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Azercadmium.Projectiles
+{
+	public class NailWeak : ModProjectile
+	{
+        public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Nail");
+        }
+		public override void SetDefaults()
+		{
+			projectile.width = 12;
+			projectile.height = 12;
+			projectile.aiStyle = 1;
+			projectile.friendly = true;
+			projectile.penetrate = 1;
+			projectile.ranged = true;
+			projectile.timeLeft = 3000;
+			projectile.ignoreWater = true;
+			aiType = 1;
+		}
+		public override void Kill(int timeLeft)
+		{
+			Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+		}
+	}   
+}
