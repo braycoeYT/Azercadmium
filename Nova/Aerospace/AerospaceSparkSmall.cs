24e03f887cd75727801b674259b7e467254d14a1
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-09-18 17:31:36 -0500

    Add files via upload

--- Nova/Aerospace/AerospaceSparkSmall.cs
+++ Nova/Aerospace/AerospaceSparkSmall.cs
@@ -29,6 +29,14 @@ namespace Azercadmium.Projectiles.Nova.Aerospace
 		}
 		public override void Kill(int timeLeft) {
 			Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+			for (int i = 0; i < 4; i++) {
+				int dustType = mod.DustType("AerospaceDust");
+				int dustIndex = Dust.NewDust(projectile.position, projectile.width, projectile.height, dustType);
+				Dust dust = Main.dust[dustIndex];
+				dust.velocity.X = dust.velocity.X + Main.rand.Next(-50, 51) * 0.01f;
+				dust.velocity.Y = dust.velocity.Y + Main.rand.Next(-50, 51) * 0.01f;
+				dust.scale *= 0.5f + Main.rand.Next(-30, 31) * 0.01f;
+			}
 		}
 	}   
 }
