650f6d13ec2892a289f7b84636855619392437ed
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-09-18 17:22:07 -0500

    Add files via upload

--- Nova/MysteriousNova.cs
+++ Nova/MysteriousNova.cs
@@ -3,6 +3,7 @@ using Terraria.ID;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 
 namespace Azercadmium.Items.Nova
 {
@@ -10,6 +11,10 @@ namespace Azercadmium.Items.Nova
 	{
 		public override void SetStaticDefaults() {
 			Tooltip.SetDefault("Sent from something powerful in deep space, this could help you in your adventure...\nNote: Still a work in progress");
+			Main.RegisterItemAnimation(item.type, new DrawAnimationVertical(5, 5)); //first is speed, second is amount of frames
+			ItemID.Sets.AnimatesAsSoul[item.type] = true;
+			ItemID.Sets.ItemIconPulse[item.type] = true;
+			ItemID.Sets.ItemNoGravity[item.type] = true;
 		}
 		public override void SetDefaults() {
 			item.width = 40;
--- Nova/Twilight/Twilight1.cs
+++ Nova/Twilight/Twilight1.cs
@@ -1,38 +1,50 @@
 using Terraria;
-using Terraria.ModLoader;
 using Terraria.ID;
-using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
 
-namespace Azercadmium.Projectiles.Nova.Twilight
+namespace Azercadmium.Items.Nova.Twilight
 {
-	public class Twilight1 : ModProjectile
+	public class Twilight1 : ModItem
 	{
-        public override void SetStaticDefaults() {
+		public override void SetStaticDefaults()  {
 			DisplayName.SetDefault("Twilight I");
-        }
+			Tooltip.SetDefault("Each boomerang fires a small crystal shard towards the cursor every two seconds");
+		}
 		public override void SetDefaults() {
-			projectile.CloneDefaults(ProjectileID.WoodenBoomerang);
-			projectile.width = 40;
-			projectile.height = 40;
-			projectile.friendly = true;
-			projectile.penetrate = 9999;
-			projectile.timeLeft = 9999;
+			item.damage = 15;
+			item.melee = true;
+			item.width = 56;
+			item.height = 56;
+			item.useTime = 32;
+			item.useAnimation = 32;
+			item.useStyle = ItemUseStyleID.HoldingUp;
+			item.knockBack = 7.9f;
+			item.value = Item.sellPrice(0, 2, 0, 0);
+			item.rare = ItemRarityID.Blue;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.useTurn = true;
+			item.shoot = mod.ProjectileType("Twilight1");
+			item.shootSpeed = 12f;
+			item.noUseGraphic = true;
 		}
-		int Timer;
-		public override void AI() {
-			Timer++;
-			projectile.timeLeft = 9999;
-			if (Timer % 120 == 60) {
-				//Main.PlaySound(SoundID.Item12);
-				Projectile.NewProjectile(projectile.Center, (projectile.DirectionTo(Main.MouseWorld)) * 10, mod.ProjectileType("TwilightCrystalSmall"), (int)(projectile.damage * 0.75f), 0, Main.myPlayer);
-			}
+		public override bool CanUseItem(Player player) {
+            for (int i = 0; i < 1000; ++i) {
+                if (Main.projectile[i].active && Main.projectile[i].owner == Main.myPlayer && Main.projectile[i].type == item.shoot) {
+                    return false;
+                }
+            }
+            return true;
+        }
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(mod.ItemType("MysteriousNova"));
+			recipe.AddIngredient(mod.ItemType("DriedEssence"));
+			recipe.AddIngredient(mod.ItemType("GlazedLens"));
+			recipe.AddIngredient(mod.ItemType("SlimyCore"));
+			recipe.AddTile(TileID.DemonAltar);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
 		}
-		public override void PostAI() {
-			if (Main.rand.NextBool()) {
-				Dust dust = Dust.NewDustDirect(projectile.position, projectile.width, projectile.height, mod.DustType("TwilightDust"));
-				dust.noGravity = true;
-				dust.scale = 1f;
-			}
-		}
-	}   
+	}
 }
