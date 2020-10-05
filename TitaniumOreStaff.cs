a578c069396515917220d7272ece3dac31841769
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-10-04 21:33:37 -0500

    Add files via upload

---
+++ TitaniumOreStaff.cs
@@ -0,0 +1,47 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Azercadmium.Items.Titanium
+{
+	public class TitaniumOreStaff : ModItem
+	{
+		public override void SetStaticDefaults() {
+			Tooltip.SetDefault("Summons a floating titanium ore to fight for you");
+			ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true;
+			ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
+		}
+		public override void SetDefaults() {
+			item.damage = 54;
+			item.knockBack = 4.85f;
+			item.mana = 10;
+			item.width = 38;
+			item.height = 38;
+			item.useTime = 36;
+			item.useAnimation = 36;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.value = Item.sellPrice(0, 2, 10, 0);
+			item.rare = ItemRarityID.LightRed;
+			item.UseSound = SoundID.Item44;
+			item.autoReuse = true;
+			item.noMelee = true;
+			item.summon = true;
+			item.buffType = BuffType<Buffs.Minions.Ores.FloatingTitaniumOre>();
+			item.shoot = ProjectileType<Projectiles.Minions.Ores.FloatingTitaniumOre>();
+		}
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
+			player.AddBuff(item.buffType, 2);
+			position = Main.MouseWorld;
+			return true;
+		}
+		public override void AddRecipes() {
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.TitaniumBar, 13);
+			recipe.AddTile(TileID.MythrilAnvil);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
