516d831fe4bed8dc0d5fd0854ddb1de892bdde91
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-12-15 21:29:50 -0600

    Add files via upload

--- Devastation/EyeoftheCosmos.cs
+++ Devastation/EyeoftheCosmos.cs
@@ -11,7 +11,7 @@ namespace Azercadmium.Items.Devastation
 	{
 		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Eye of the Cosmos");
-			Tooltip.SetDefault("Used to activate and deactivate Devastation Mode.\nNote: Heavily unfinished.\nCan only be activated in Expert Mode and when no bosses are alive.\nDevastation mode only items will have the Mint Chocolate rarity.\nCheck the Azercadmium Discord Server in #info for full list of changes.");
+			Tooltip.SetDefault("Used to activate and deactivate Devastation Mode\nDevastation mode heavily increases the difficulty of the game and is not recommended for a first playthrough\nNote: Heavily unfinished\nCan only be activated in Expert Mode and when no bosses are alive\nDevastation mode only items will have the Mint Chocolate rarity\nCheck the Azercadmium Discord Server in #info for full list of changes\nDue to an odd glitch, after fighting a boss, you may or may not have to reload your world to change the difficulty again");
 			ItemID.Sets.SortingPriorityBossSpawns[item.type] = 13;
 		}
 		public override void SetDefaults() {
@@ -24,8 +24,7 @@ namespace Azercadmium.Items.Devastation
 			item.useStyle = ItemUseStyleID.HoldingUp;
 			item.consumable = false;
 		}
-		public bool BossAlive()
-		{
+		public bool BossAlive() {
 			for (int i = 0; i < Main.maxNPCs; i++) {
 				if (Main.npc[i].boss || Main.npc[i].type == NPCID.EaterofWorldsHead) {
 					return true;
